15b863438b960507d720c88ab435b3cf6f8bb3f8
Magui <magui@gmail.com>
2023-04-12 18:07:44 -0300

    agrego ejercicio.py

---
+++ ejercicio.py
@@ -0,0 +1,11 @@
+productos = [
+    { 'nombre': 'jabon', 'id': 1},
+    { 'nombre': 'shampoo', 'id': 2},
+    { 'nombre': 'crema de enguaje', 'id': 3},
+    { 'nombre': 'acondicionador', 'id': 4}
+]
+
+
+# necesito que imprimas los nombres de los productos
+
+# y los agregues a otra lista ordenados por nombre
